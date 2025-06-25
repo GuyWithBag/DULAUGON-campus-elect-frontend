30db72f4eae3e05b052fb910a2d38c8e8d758dee
GuyWithBag <ldulaugon123@gmail.com>
2025-06-25 13:40:22 +0800

    feat: changed font and size of header1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Loejee Miguel Dulaugon</h1>
+        <h1 className="text-5xl font-[family-name:var(--font-geist-mono)] font-bold">Loejee Miguel Dulaugon</h1>
         <StudentsList />
         <Image
           className="dark:invert"
@@ -25,7 +25,6 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
-            Kurt Montes
           </li>
         </ol>
 
