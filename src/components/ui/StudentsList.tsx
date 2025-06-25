95151daa1432757ff42066f26025c90b08e439a2
GuyWithBag <ldulaugon123@gmail.com>
2025-06-25 14:07:30 +0800

    test

--- src/components/ui/StudentsList.tsx
+++ src/components/ui/StudentsList.tsx
@@ -10,7 +10,7 @@ export type StudentType = {
 
 const StudentsList = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/students`
+    ``
   );
 
   const students: StudentType[] = await res.json();
